fc16e578ee30988b9dbf1bc21ddeeb279c5da4e4
Billu27 <153723738+Billu27@users.noreply.github.com>
2024-01-11 11:46:44 +0530

    calcaulator havk

---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        outxbx.Text = outxbx.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        outxbx.Text = outxbx.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        outxbx.Text = outxbx.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        outxbx.Text = outxbx.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        outxbx.Text = outxbx.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        outxbx.Text = outxbx.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        outxbx.Text = outxbx.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        outxbx.Text = outxbx.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        outxbx.Text = outxbx.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        outxbx.Text = outxbx.Text + "0"
+    End Sub
+
+    Private Sub btnminus_Click(sender As Object, e As EventArgs) Handles btnminus.Click
+
+    End Sub
+
+    Private Sub outxbx_TextChanged(sender As Object, e As EventArgs) Handles outxbx.TextChanged
+
+    End Sub
+End Class
