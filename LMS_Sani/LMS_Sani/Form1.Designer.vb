46f05cfc84b4a8ad8609b74f5aa2907d986e0a89
Billu27 <153723738+Billu27@users.noreply.github.com>
2024-02-07 15:22:15 +0530

    7th february 2024 UID
    mysql connenctions

---
+++ LMS_Sani/LMS_Sani/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BookIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AuthorDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.YearDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BooksBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Library_saniDataSet = New LMS_Sani.library_saniDataSet()
+        Me.BooksTableAdapter = New LMS_Sani.library_saniDataSetTableAdapters.booksTableAdapter()
+        Me.Name_txbx = New System.Windows.Forms.TextBox()
+        Me.author_txbx = New System.Windows.Forms.TextBox()
+        Me.year_txbx = New System.Windows.Forms.TextBox()
+        Me.Delete_txbx = New System.Windows.Forms.TextBox()
+        Me.Add_Btn = New System.Windows.Forms.Button()
+        Me.del_Btn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BooksBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Library_saniDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(34, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(378, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "LIBRARY MANAGEMENT SYSTEM"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.BookIDDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.AuthorDataGridViewTextBoxColumn, Me.YearDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.BooksBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(20, 55)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(392, 150)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'BookIDDataGridViewTextBoxColumn
+        '
+        Me.BookIDDataGridViewTextBoxColumn.DataPropertyName = "BookID"
+        Me.BookIDDataGridViewTextBoxColumn.HeaderText = "BookID"
+        Me.BookIDDataGridViewTextBoxColumn.Name = "BookIDDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'AuthorDataGridViewTextBoxColumn
+        '
+        Me.AuthorDataGridViewTextBoxColumn.DataPropertyName = "Author"
+        Me.AuthorDataGridViewTextBoxColumn.HeaderText = "Author"
+        Me.AuthorDataGridViewTextBoxColumn.Name = "AuthorDataGridViewTextBoxColumn"
+        '
+        'YearDataGridViewTextBoxColumn
+        '
+        Me.YearDataGridViewTextBoxColumn.DataPropertyName = "Year"
+        Me.YearDataGridViewTextBoxColumn.HeaderText = "Year"
+        Me.YearDataGridViewTextBoxColumn.Name = "YearDataGridViewTextBoxColumn"
+        '
+        'BooksBindingSource
+        '
+        Me.BooksBindingSource.DataMember = "books"
+        Me.BooksBindingSource.DataSource = Me.Library_saniDataSet
+        '
+        'Library_saniDataSet
+        '
+        Me.Library_saniDataSet.DataSetName = "library_saniDataSet"
+        Me.Library_saniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'BooksTableAdapter
+        '
+        Me.BooksTableAdapter.ClearBeforeFill = True
+        '
+        'Name_txbx
+        '
+        Me.Name_txbx.Location = New System.Drawing.Point(156, 234)
+        Me.Name_txbx.Name = "Name_txbx"
+        Me.Name_txbx.Size = New System.Drawing.Size(100, 20)
+        Me.Name_txbx.TabIndex = 2
+        '
+        'author_txbx
+        '
+        Me.author_txbx.Location = New System.Drawing.Point(156, 271)
+        Me.author_txbx.Name = "author_txbx"
+        Me.author_txbx.Size = New System.Drawing.Size(100, 20)
+        Me.author_txbx.TabIndex = 3
+        '
+        'year_txbx
+        '
+        Me.year_txbx.Location = New System.Drawing.Point(156, 308)
+        Me.year_txbx.Name = "year_txbx"
+        Me.year_txbx.Size = New System.Drawing.Size(100, 20)
+        Me.year_txbx.TabIndex = 4
+        '
+        'Delete_txbx
+        '
+        Me.Delete_txbx.Location = New System.Drawing.Point(156, 374)
+        Me.Delete_txbx.Name = "Delete_txbx"
+        Me.Delete_txbx.Size = New System.Drawing.Size(100, 20)
+        Me.Delete_txbx.TabIndex = 5
+        '
+        'Add_Btn
+        '
+        Me.Add_Btn.Location = New System.Drawing.Point(312, 271)
+        Me.Add_Btn.Name = "Add_Btn"
+        Me.Add_Btn.Size = New System.Drawing.Size(75, 23)
+        Me.Add_Btn.TabIndex = 6
+        Me.Add_Btn.Text = "ADD"
+        Me.Add_Btn.UseVisualStyleBackColor = True
+        '
+        'del_Btn
+        '
+        Me.del_Btn.Location = New System.Drawing.Point(312, 374)
+        Me.del_Btn.Name = "del_Btn"
+        Me.del_Btn.Size = New System.Drawing.Size(75, 23)
+        Me.del_Btn.TabIndex = 7
+        Me.del_Btn.Text = "DELETE"
+        Me.del_Btn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(55, 237)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Name : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(55, 278)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Author : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(55, 312)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Year : "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(51, 372)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Book ID"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(119, 347)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(173, 24)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "DELTET A BOOK"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 407)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.del_Btn)
+        Me.Controls.Add(Me.Add_Btn)
+        Me.Controls.Add(Me.Delete_txbx)
+        Me.Controls.Add(Me.year_txbx)
+        Me.Controls.Add(Me.author_txbx)
+        Me.Controls.Add(Me.Name_txbx)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BooksBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Library_saniDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Library_saniDataSet As LMS_Sani.library_saniDataSet
+    Friend WithEvents BooksBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents BooksTableAdapter As LMS_Sani.library_saniDataSetTableAdapters.booksTableAdapter
+    Friend WithEvents BookIDDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents AuthorDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents YearDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Name_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents author_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents year_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents Delete_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents Add_Btn As System.Windows.Forms.Button
+    Friend WithEvents del_Btn As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ LMS_Sani/LMS_Sani/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Library_saniDataSet.books' table. You can move, or remove it, as needed.
+        Me.BooksTableAdapter.Fill(Me.Library_saniDataSet.books)
+
+    End Sub
+
+    Private Sub Add_Btn_Click(sender As Object, e As EventArgs) Handles Add_Btn.Click
+        Dim Query As String
+        'step 1: connect with the database
+        Dim Con As MySqlConnection = New MySqlConnection("DataSource=localhost;Database=library_sani;User Id=root;Password=;")
+        Con.Open()
+        'step 2: Create query string
+        Query = " insert into books (Name, Author, Year) values ('" + Name_txbx.Text + "','" + author_txbx.Text + "'," + year_txbx.Text + ");"
+
+        'step 3: run the query
+        Dim c As MySqlCommand = New MySqlCommand(Query, Con)
+        Dim i As Integer = c.ExecuteNonQuery()
+
+        ' step 4: check the result
+        If (i = 0) Then
+            MsgBox("fail")
+        Else
+            MsgBox("sucess")
+            Me.BooksTableAdapter.Fill(Me.Library_saniDataSet.books)
+        End If
+        'step 5: close connection
+        Con.Close()
+    End Sub
+
+
+    Private Sub del_Btn_Click(sender As Object, e As EventArgs) Handles del_Btn.Click
+        Dim Query As String
+        'step 1: connect with the database
+        Dim Con As MySqlConnection = New MySqlConnection("DataSource=localhost;Database=library_sani;User Id=root;Password=;")
+        Con.Open()
+        'step 2: Create query string
+        Query = " DELETE from books where BookID = " + Delete_txbx.Text + ";"
+
+        'step 3: run the query
+        Dim c As MySqlCommand = New MySqlCommand(Query, Con)
+        Dim i As Integer = c.ExecuteNonQuery()
+
+        ' step 4: check the result
+        If (i = 0) Then
+            MsgBox("fail")
+        Else
+            MsgBox("sucess")
+            Me.BooksTableAdapter.Fill(Me.Library_saniDataSet.books)
+        End If
+        'step 5: close connection
+        Con.Close()
+    End Sub
+End Class
