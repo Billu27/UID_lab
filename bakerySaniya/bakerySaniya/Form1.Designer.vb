d6627f77f4e355e74c1574f35c0a84788908f7de
Billu27 <153723738+Billu27@users.noreply.github.com>
2024-04-03 16:31:50 +0530

    SCREENSHOTS

---
+++ bakerySaniya/bakerySaniya/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BakerysaniDataSet = New bakerySaniya.bakerysaniDataSet()
+        Me.BakeryitemsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Bakery_itemsTableAdapter = New bakerySaniya.bakerysaniDataSetTableAdapters.bakery_itemsTableAdapter()
+        Me.ItemidDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QuantityDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DescriptionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PriceDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.itemID_txbx = New System.Windows.Forms.TextBox()
+        Me.qty_txbx = New System.Windows.Forms.TextBox()
+        Me.Price_txbx = New System.Windows.Forms.TextBox()
+        Me.upd_btn = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BakerysaniDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BakeryitemsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-23, 230)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(181, 196)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(499, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(81, 54)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.HotPink
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Panel1.Location = New System.Drawing.Point(-1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(583, 54)
+        Me.Panel1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.HotPink
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Snow
+        Me.Label1.Location = New System.Drawing.Point(97, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(387, 51)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SWEET BAKERY CO"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.LightPink
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ItemidDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.QuantityDataGridViewTextBoxColumn, Me.DescriptionDataGridViewTextBoxColumn, Me.PriceDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.BakeryitemsBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 69)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(557, 170)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'BakerysaniDataSet
+        '
+        Me.BakerysaniDataSet.DataSetName = "bakerysaniDataSet"
+        Me.BakerysaniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'BakeryitemsBindingSource
+        '
+        Me.BakeryitemsBindingSource.DataMember = "bakery_items"
+        Me.BakeryitemsBindingSource.DataSource = Me.BakerysaniDataSet
+        '
+        'Bakery_itemsTableAdapter
+        '
+        Me.Bakery_itemsTableAdapter.ClearBeforeFill = True
+        '
+        'ItemidDataGridViewTextBoxColumn
+        '
+        Me.ItemidDataGridViewTextBoxColumn.DataPropertyName = "item _id"
+        Me.ItemidDataGridViewTextBoxColumn.HeaderText = "item _id"
+        Me.ItemidDataGridViewTextBoxColumn.Name = "ItemidDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'QuantityDataGridViewTextBoxColumn
+        '
+        Me.QuantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.HeaderText = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.Name = "QuantityDataGridViewTextBoxColumn"
+        '
+        'DescriptionDataGridViewTextBoxColumn
+        '
+        Me.DescriptionDataGridViewTextBoxColumn.DataPropertyName = "Description"
+        Me.DescriptionDataGridViewTextBoxColumn.HeaderText = "Description"
+        Me.DescriptionDataGridViewTextBoxColumn.Name = "DescriptionDataGridViewTextBoxColumn"
+        '
+        'PriceDataGridViewTextBoxColumn
+        '
+        Me.PriceDataGridViewTextBoxColumn.DataPropertyName = "Price"
+        Me.PriceDataGridViewTextBoxColumn.HeaderText = "Price"
+        Me.PriceDataGridViewTextBoxColumn.Name = "PriceDataGridViewTextBoxColumn"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Indigo
+        Me.Label2.Location = New System.Drawing.Point(173, 257)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 25)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Item ID : "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Indigo
+        Me.Label3.Location = New System.Drawing.Point(162, 300)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 25)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Quantity : "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Indigo
+        Me.Label4.Location = New System.Drawing.Point(196, 341)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Price : "
+        '
+        'itemID_txbx
+        '
+        Me.itemID_txbx.BackColor = System.Drawing.Color.Thistle
+        Me.itemID_txbx.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.itemID_txbx.Location = New System.Drawing.Point(276, 262)
+        Me.itemID_txbx.Name = "itemID_txbx"
+        Me.itemID_txbx.Size = New System.Drawing.Size(173, 20)
+        Me.itemID_txbx.TabIndex = 7
+        '
+        'qty_txbx
+        '
+        Me.qty_txbx.BackColor = System.Drawing.Color.Thistle
+        Me.qty_txbx.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.qty_txbx.Location = New System.Drawing.Point(276, 306)
+        Me.qty_txbx.Name = "qty_txbx"
+        Me.qty_txbx.Size = New System.Drawing.Size(173, 20)
+        Me.qty_txbx.TabIndex = 8
+        '
+        'Price_txbx
+        '
+        Me.Price_txbx.BackColor = System.Drawing.Color.Thistle
+        Me.Price_txbx.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.Price_txbx.Location = New System.Drawing.Point(276, 346)
+        Me.Price_txbx.Name = "Price_txbx"
+        Me.Price_txbx.Size = New System.Drawing.Size(173, 20)
+        Me.Price_txbx.TabIndex = 9
+        '
+        'upd_btn
+        '
+        Me.upd_btn.BackColor = System.Drawing.Color.HotPink
+        Me.upd_btn.Location = New System.Drawing.Point(315, 372)
+        Me.upd_btn.Name = "upd_btn"
+        Me.upd_btn.Size = New System.Drawing.Size(89, 31)
+        Me.upd_btn.TabIndex = 10
+        Me.upd_btn.Text = "UPDATE"
+        Me.upd_btn.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightYellow
+        Me.ClientSize = New System.Drawing.Size(582, 415)
+        Me.Controls.Add(Me.upd_btn)
+        Me.Controls.Add(Me.Price_txbx)
+        Me.Controls.Add(Me.qty_txbx)
+        Me.Controls.Add(Me.itemID_txbx)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BakerysaniDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BakeryitemsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents BakerysaniDataSet As bakerySaniya.bakerysaniDataSet
+    Friend WithEvents BakeryitemsBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents Bakery_itemsTableAdapter As bakerySaniya.bakerysaniDataSetTableAdapters.bakery_itemsTableAdapter
+    Friend WithEvents ItemidDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents QuantityDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DescriptionDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PriceDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents itemID_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents qty_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents Price_txbx As System.Windows.Forms.TextBox
+    Friend WithEvents upd_btn As System.Windows.Forms.Button
+
+End Class
---
+++ bakerySaniya/bakerySaniya/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Public Sub UpdateQuantity(itemId As Integer, newQuantity As Integer)
+        Dim connectionString As String = "server=localhost;database=bakerysani;user id=root;password=student;"
+        Dim connection As New MySqlConnection(connectionString)
+        connection.Open()
+        Dim query As String = "UPDATE Bakery_items SET Quantity = @newQuantity WHERE item_id = @itemId"
+
+
+        Using command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@newQuantity", newQuantity)
+            command.Parameters.AddWithValue("@itemId", itemId)
+
+            Try
+
+                Dim rowsAffected As Integer = command.ExecuteNonQuery()
+                Console.WriteLine("Rows affected: " & rowsAffected.ToString())
+                Me.Bakery_itemsTableAdapter.Fill(Me.BakerysaniDataSet.bakery_items)
+            Catch ex As Exception
+                Console.WriteLine("An error occurred: " & ex.Message)
+                connection.Close()
+            End Try
+        End Using
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BakerysaniDataSet.bakery_items' table. You can move, or remove it, as needed.
+        Me.Bakery_itemsTableAdapter.Fill(Me.BakerysaniDataSet.bakery_items)
+
+    End Sub
+
+    Private Sub upd_btn_Click(sender As Object, e As EventArgs) Handles upd_btn.Click
+        UpdateQuantity(Val(itemID_txbx.Text), Val(qty_txbx.Text))
+    End Sub
+
+End Class
